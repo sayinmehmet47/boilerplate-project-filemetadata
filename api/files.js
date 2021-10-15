96923b74885a4a3db9129fd2f2111c4d54945158
sayin47 <sayinmehmet47@gmail.com>
2021-10-15 23:51:03 +0300

    uploads edited

--- api/files.js
+++ api/files.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: '../public/data/uploads' });
+const upload = multer({ dest: './public/data/uploads' });
 const MyFiles = require('../models/Files.js');
 
 router.post('/', upload.single('uploaded_file'), function (req, res) {
