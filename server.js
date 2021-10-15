326430690aff43e45756c429fdefad3f6fcfe38d
sayin47 <sayinmehmet47@gmail.com>
2021-10-15 17:04:18 +0300

    server created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+// const mongoose = require('mongoose');
+const cors = require('cors');
+const { urlencoded } = require('body-parser');
+
+require('dotenv').config();
+
+// mongoose.connect(process.env.MONGO_URI).then(console.log('connected'));
+const fileanalyse = require('./api/files.js');
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/api/fileanalyse', fileanalyse);
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
