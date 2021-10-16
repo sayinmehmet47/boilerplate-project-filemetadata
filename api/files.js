e0d245c6f4819f6f29cf834ea0ffc9135f7ec93a
sayin47 <sayinmehmet47@gmail.com>
2021-10-16 17:20:04 +0300

    vercel added

--- api/files.js
+++ api/files.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: './public/data/uploads' });
 const MyFiles = require('../models/Files.js');
+
+const storage = multer.diskStorage({
+  destination: './public/data/uploads/', // use it when upload
+  filename: (req, file, cb) => {
+    // nameFile = file.originalname + " "+ Date.now() // --> give "video.mp4 1622180824748"
+    let [filename, extension] = file.originalname.split('.');
+    let nameFile = filename + '-' + Date.now() + '.' + extension; // --> give "video-1622181268053.mp4"
+    cb(null, nameFile);
+  },
+});
+const upload = multer({ storage: storage });
 
 router.post('/', upload.single('upfile'), function (req, res) {
   // req.file is the name of your file in the form above, here 'uploaded_file'
